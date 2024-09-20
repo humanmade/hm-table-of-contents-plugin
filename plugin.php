48bd677703fa5d5dccafc4858d05491fd963ab00
Matthew Haines-Young <matthewhainesyoung@gmail.com>
2024-09-20 10:52:01 +0100

    Bump version

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@
  * Description: Add anchors to headings in post content and generate table of contents.
  * Author: Human Made Limited
  * Author URI: https://humanmade.com/
- * Version: 1.1.0
+ * Version: 1.0.3
  */
 
 namespace HM\TOC;
